e5d71ecda92a03e19a6102dc1ae9d9292e9dc956
Charly <xhookmandev@gmail.com>
2023-09-02 02:38:25 +0200

    Can save the patched method to the correct smali file

--- src/main/java/com/chacha/igexperimentspatcher/Patcher.java
+++ src/main/java/com/chacha/igexperimentspatcher/Patcher.java
@@ -1,13 +1,11 @@
 package com.chacha.igexperimentspatcher;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class Patcher {
     private final String pathToApk;
+    private String methodToPatch;
     public Patcher(String pathToApk){
         System.out.println("patcher constructor");
         this.pathToApk = pathToApk;
@@ -22,14 +20,23 @@ public class Patcher {
             apkUtils.decompile(pathToApk);
 
         List<File> f = apkUtils.getFileForExperiments();
-        System.out.println("Found experiments file: " + f);
+       // System.out.println("Found experiments file: " + f);
         try {
+            //DEBUG
+            //File patchFile = new File("ig.apk.out/smali_classes5/X/Bh4.smali");
             enableExperiments(f.get(0));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    // trouver et renvoyer le nom de la méthode à patcher,
+    // chercher dans le fichier smali la méthode complète avec ce nom
+    // remplacer la ligne avec le regex par const/4 v0, 0x1
+
+    private String extractMethodName() {
+        return methodToPatch;
+    }
     private String searchMethodContentForExperiments(String filePath) throws IOException {
         boolean inMethod = false;
         StringBuilder methodContent = new StringBuilder();
@@ -37,14 +44,13 @@ public class Patcher {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                if (line.trim().startsWith(".method")) {
+                if (line.trim().startsWith(".method public static")) {
                     // Start of a new method
+                    methodToPatch = line;
                     inMethod = true;
-                    line.trim();
                     methodContent.setLength(0); // Clear the method content
                 } else if (inMethod) {
                     methodContent.append(line).append("\n");
-
                     if (line.trim().equals(".end method")) {
                         // End of the method
                         inMethod = false;
@@ -60,19 +66,72 @@ public class Patcher {
         System.out.println("No method found for experiments");
         return null;
     }
-    private void enableExperiments(File file) throws IOException {
-        System.out.println("Enabling experiments...");
-        String methodContent = searchMethodContentForExperiments(file.getAbsolutePath());
-        if(methodContent == null) {
-            System.out.println("No method content found for experiments");
+
+    private void replaceMethodInFile(String filePath) throws IOException {
+        String methodContent = searchMethodContentForExperiments(filePath);
+        boolean inMethod = false;
+        if (methodContent == null) {
+            System.out.println("No method found for experiments");
             return;
         }
+        System.out.println("Method content: " + methodContent);
+        String methodToPatch = extractMethodName();
+        StringBuilder originalContent = new StringBuilder();
 
-        methodContent = methodContent.replaceAll("invoke-static \\{p1}, LX/[A-Z0-9]+;->A0[0-9]+\\(.+?\\)Z", "const/4 v0, 0x1");
-        System.out.println("Patched method content:\n");
-        System.out.println(methodContent);
+        System.out.println("Method to patch: " + methodToPatch);
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().startsWith(methodToPatch)) {
+                    // Start of a new method
+                        inMethod = true;
+                        originalContent.append(line).append("\n");
+                    } else if (inMethod) {
+                    System.out.println("Actual line: " + line);
+
+                    if (line.matches("\\s*invoke-static .*LX/[A-Za-z0-9/;]+->[A-Za-z0-9]+\\(.*\\)Z")) {
+                  //  if(line.contains("invoke-static {p1}, LX/1D4;->A00(LX/0e2;)Z")){ // DEBUG
+                        System.out.println("Found line to patch");
+                        originalContent.append("\tconst/4 v0, 0x1").append("\n");
+                    } else {
+                        originalContent.append(line).append("\n");
+                    }
+
+                    if (line.trim().equals(".end method")) {
+                        // End of the method
+                        inMethod = false;
+                    }
+                } else {
+                    originalContent.append(line).append("\n");
+                }
+            }
+
+            // Write the modified content back to the Smali file
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                writer.write(originalContent.toString());
+            }
+
+            System.out.println("Line replaced successfully.");
+        }
+    }
+
+    private void enableExperiments(File file) throws IOException {
+        System.out.println("Enabling experiments...");
+        //String methodContent = searchMethodContentForExperiments(file.getAbsolutePath());
+       /* if(methodContent == null) {
+            System.out.println("No method content found for experiments");
+            return;
+        }*/
+
+       // methodContent = methodContent.replaceAll("invoke-static \\{p1}, LX/[A-Z0-9]+;->A0[0-9]+\\(.+?\\)Z", "const/4 v0, 0x1");
+       // System.out.println("Patched method content:\n");
+       // System.out.println(methodContent);
         /* Example of the line in smali code:
                invoke-static {p1}, LX/1D4;->A00(LX/0e2;)Z
          */
+
+
+        replaceMethodInFile(file.getAbsolutePath());
+        System.out.println("Experiments enabled successfully.");
     }
 }
